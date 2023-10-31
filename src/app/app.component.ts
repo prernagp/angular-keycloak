9d7a3455eb5fd00eb54f4fea1b876c92473022bb
prernagp <prernagp90@gmail.com>
2023-10-31 16:26:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  hitLogin() {
+    return axios.post('http://localhost:3001/auth/keycloak', 
+    {
+      'client_id': 'android',
+      'client_secret': 'ymCr1fSfodHbgh5z7c9dH5kJ7FQ5GwWr'
+    },
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+  }
 }
+
